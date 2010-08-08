166bb443b57f7900d2ef3ca3d1d41d61126b04e3
Tim Freeman <tim@timfreeman.org>
2010-08-08 16:16:03 -0500

    New runlogs directory for to put logfiles from instances

--- src/python/epucontrol/sbin/wipe_state.py
+++ src/python/epucontrol/sbin/wipe_state.py
@@ -18,6 +18,13 @@ def get_persistencedir(p):
         sys.stderr.write("There is no persistencedir configuration")
         return None
     return sbin_common.apply_vardir_maybe(p, persistencedir)
+
+def get_runlogdir(p):
+    runlogdir = p.get_conf_or_none("events", "runlogdir")
+    if not runlogdir:
+        sys.stderr.write("There is no runlogdir configuration")
+        return None
+    return sbin_common.apply_vardir_maybe(p, runlogdir)
     
 if len(sys.argv) != 2:
     sys.stderr.write("This program requires 1 argument, the absolute path to the main.conf file")
@@ -33,13 +40,26 @@ persistencedir = get_persistencedir(p)
 if not persistencedir:
     sys.exit(1)
 
+runlogdir = get_runlogdir(p)
+if not runlogdir:
+    sys.exit(1)
+    
+runlogdirs = []
+for root, dirs, files in os.walk(runlogdir):
+    for adir in dirs:
+        path = os.path.join(runlogdir, adir)
+        runlogdirs.append(path)
+
 # find the newest file in the directory:
 
 sys.stderr.write("Log file dir:    %s\n" % logfiledir)
 sys.stderr.write("Persistence dir: %s\n" % persistencedir)
-
+sys.stderr.write("Runlog dir:      %s\n" % runlogdir)
+for adir in runlogdirs:
+    sys.stderr.write("    subdir:      %s\n" % adir)
+    
 try:
-    yn = sbin_common.get_user_input("Delete all log and persistence files? y/n")
+    yn = sbin_common.get_user_input("\nDelete all these files? Are you sure?? y/n")
     yn = yn.lower()
 except KeyboardInterrupt:
     sys.stderr.flush()
@@ -50,6 +70,8 @@ if yn != "y" and yn != "yes":
     sys.stderr.flush()
     sys.stderr.write("\n\nExiting, nothing was deleted.\n")
     sys.exit(2)
+
+print ""
 
 for root, dirs, files in os.walk(logfiledir):
     for name in files:
@@ -72,3 +94,14 @@ for root, dirs, files in os.walk(persistencedir):
             sys.stderr.write("Deleting: %s\n" % path)
             os.remove(path)
     break # only look in the top directory
+
+for adir in runlogdirs:
+    for root, dirs, files in os.walk(adir):
+        for name in files:
+            path = os.path.join(adir, name)
+            if os.path.isfile(path):
+                sys.stderr.write("Deleting: %s\n" % path)
+                os.remove(path)
+        break # only look in the top directory
+    sys.stderr.write("Deleting directory: %s\n" % adir)
+    os.rmdir(adir)
