76efb28cfb9e31015af0450b5589c60f672010b8
Tim Freeman <tim@timfreeman.org>
2010-08-13 18:50:40 -0500

    fixes hostname discovery when worker N > 1

--- src/python/epucontrol/main/ec_core_findworkers.py
+++ src/python/epucontrol/main/ec_core_findworkers.py
@@ -12,12 +12,18 @@ def find(p, c, m, action, run_name, once=False):
     while True:
         ec_core_logfetch.fetch_by_service_name(p, c, m, run_name, PROVISIONER)
         ec_core_eventgather.update_events(p, c, m, run_name)
-        launched_vms = vms_launched_by_provisioner(p, c, m, run_name)
         
+        # order is important, first "new_node"
+        launched_vms = vms_launched(p, c, m, run_name, "new_node")
         for vm in launched_vms:
             if m.persistence.new_vm_maybe(run_name, vm):
                 c.log.info("Found new worker: %s : %s" 
                             % (vm.instanceid, vm.hostname))
+                
+        # then "node_started"
+        launched_vms = vms_launched(p, c, m, run_name, "node_started")
+        for vm in launched_vms:
+            m.persistence.new_vm_maybe(run_name, vm)
         
         allvms = m.persistence.get_run_vms_or_none(run_name)
         c.log.debug("Know of %d VMs in run '%s'" % (len(allvms), run_name))
@@ -25,7 +31,25 @@ def find(p, c, m, action, run_name, once=False):
             break
         time.sleep(15)
 
-def vms_launched_by_provisioner(p, c, m, run_name):
+def vms_launched(p, c, m, run_name, eventname):
+    provisioner = _get_provisioner(p, c, m, run_name)
+    vms = []
+    for event in provisioner.events:
+        if event.name == eventname:
+            vm = RunVM()
+            if eventname == "new_node":
+                vm.instanceid = event.extra['iaas_id']
+            elif eventname == "node_started":
+                vm.instanceid = event.extra['node_id']
+            else:
+                raise IncompatibleEnvironment()
+            vm.hostname = event.extra['public_ip']
+            vm.service_type = "unknown" + vm.WORKER_SUFFIX
+            m.runlogs.new_vm(vm)
+            vms.append(vm)
+    return vms
+
+def _get_provisioner(p, c, m, run_name):
     allvms = m.persistence.get_run_vms_or_none(run_name)
     provisioner = None
     for vm in allvms:
@@ -33,17 +57,4 @@ def vms_launched_by_provisioner(p, c, m, run_name):
             provisioner = vm
     if not provisioner:
         raise IncompatibleEnvironment("Cannot find a record of the provisioner for run '%s'" % run_name)
-    
-    vms = []
-    for event in provisioner.events:
-        if event.name == "new_node":
-            vm = RunVM()
-            vm.instanceid = event.extra['iaas_id']
-            vm.hostname = event.extra['public_ip']
-            vm.service_type = "unknown" + vm.WORKER_SUFFIX
-            m.runlogs.new_vm(vm)
-            vms.append(vm)
-        elif event.name == "node_started":
-            vm.hostname = event.extra['public_ip']
-            m.persistence.new_vm_maybe(run_name, vm)
-    return vms
+    return provisioner
