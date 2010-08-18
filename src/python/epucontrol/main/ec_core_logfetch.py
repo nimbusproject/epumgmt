7db9cc6acfc3115a63eefa005012f989a368058e
Tim Freeman <tim@timfreeman.org>
2010-08-17 21:27:24 -0500

    logfetch should use threads too, this makes both logfetch and killrun much faster

--- src/python/epucontrol/main/ec_core_logfetch.py
+++ src/python/epucontrol/main/ec_core_logfetch.py
@@ -1,4 +1,27 @@
 from epucontrol.api.exceptions import *
+try:
+    from threading import Thread
+except ImportError:
+    from dummy_threading import Thread
+
+class FetchThread(Thread):
+    
+    def __init__ (self, worker, c, m):
+        Thread.__init__(self)
+        self.worker = worker
+        self.iid = worker.instanceid # convenience
+        self.c = c
+        self.m = m
+        self.error = None
+        
+    def run(self):
+        try:
+            #self.c.log.debug("fetching logs from '%s'" % self.iid)
+            self.m.runlogs.fetch_logs(self.worker, self.m)
+            self.c.log.info("Fetched logs from '%s'" % self.iid)
+        except Exception,e:
+            self.c.log.error("error retrieving logs from '%s'" % self.iid)
+            self.error = e
 
 def fetch_all(p, c, m, run_name):
     """Fetch log files from any VM instance that is part of the run
@@ -10,9 +33,34 @@ def fetch_all(p, c, m, run_name):
         c.log.debug("fetch_all()")
     
     run_vms = _get_runvms_required(m, run_name)
-        
+    
+    threads = []
     for vm in run_vms:
-        _fetch_one_vm(p, c, m, run_name, vm)
+        threads.append(FetchThread(vm, c, m))
+    
+    txt = "%d VM" % len(run_vms)
+    if len(run_vms) != 1:
+        txt += "s"
+    c.log.info("Beginning to logfetch %s" % txt)
+    
+    for thr in threads:
+        thr.start()
+       
+    for thr in threads:
+        thr.join()
+        
+    error_count = 0
+    for thr in threads:
+        if thr.error:
+            error_count += 1
+            msg = "** Issue with %s:\n" % thr.worker.instanceid
+            msg += str(thr.error)
+            c.log.error("\n\n%s\n" % msg)
+    
+    if error_count:
+        c.log.info("All fetched with %d errors" % error_count)
+    else:
+        c.log.info("All fetched")
 
 def fetch_by_vm_id(p, c, m, run_name, instanceid):
     """Fetch log files from the VM instance in this run with a particular ID
