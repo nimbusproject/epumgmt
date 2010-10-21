ae9a2ee48f2aeaa9ab602276dc5ea72bbd0e9a29
Tim Freeman <tim@timfreeman.org>
2010-10-21 16:38:11 -0500

    What is old is now new

--- libexec/epumgmt/fab-bootstrap/chefconf.rb
+++ libexec/epumgmt/fab-bootstrap/chefconf.rb
@@ -1,5 +1,5 @@
-cookbook_path "/opt/dt-data/cookbooks"
+cookbook_path "/opt/chef/cookbooks"
 log_level :info
-file_store_path "/opt/dt-data/tmp"
-file_cache_path "/opt/dt-data/tmp"
+file_store_path "/opt/chef/tmp"
+file_cache_path "/opt/chef/tmp"
 Chef::Log::Formatter.show_time = false
