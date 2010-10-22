72f78ff87f1c83312632affe17a6851d3878b727
Tim Freeman <tim@timfreeman.org>
2010-10-22 04:07:48 -0500

    not ignoring DB file in best way

--- bin/epumgmt.sh
+++ bin/epumgmt.sh
@@ -5,6 +5,11 @@ PYTHON_EXE="/usr/bin/env python"
 NIMBUS_CONTROL_DIR_REL="`dirname $0`/.."
 NIMBUS_CONTROL_DIR=`cd $NIMBUS_CONTROL_DIR_REL; pwd`
 
+default_dbfile="$NIMBUS_CONTROL_DIR/var/epumgmt/persistence/epumgmt.db"
+if [ ! -f $default_dbfile ]; then
+    touch $default_dbfile
+fi
+
 NIMBUS_CONTROL_MAINCONF="$NIMBUS_CONTROL_DIR/etc/epumgmt/main.conf"
 
 needsconf="y"
