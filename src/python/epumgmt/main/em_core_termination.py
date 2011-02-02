0c45bee9d248992acb53f0b0e5dd2fcbf4959519
Tim Freeman <tim@timfreeman.org>
2011-02-01 18:54:39 -0600

    killrun (re)implementation

--- src/python/epumgmt/main/em_core_termination.py
+++ src/python/epumgmt/main/em_core_termination.py
@@ -1,4 +1,7 @@
 from epumgmt.api.exceptions import *
+import epumgmt.defaults.child as child
+from epumgmt.main.em_core_load import get_cloudinit
+import time
 
 def terminate(p, c, m, run_name):
     """Destroy all VM instances that are part of the run.
@@ -8,18 +11,58 @@ def terminate(p, c, m, run_name):
 
     if c.trace:
         c.log.debug("terminate()")
-    
-    run_vms = m.persistence.get_run_vms_or_none(run_name)
-    if not run_vms or len(run_vms) == 0:
-        raise IncompatibleEnvironment("Cannot find any VMs associated with run '%s'" % run_name)
-        
-    for vm in run_vms:
-        c.log.info("Terminating '%s', Host '%s'" % (vm.instanceid, vm.hostname))
-   
-    provisioner_instanceids = [vm.instanceid for vm in run_vms if vm.service_type == "provisioner"]
-    instanceids = [vm.instanceid for vm in run_vms if vm.service_type != "provisioner"]
 
-    # TODO
-    raise ProgrammingError("Not implemented yet. This will terminate workers via call to the provisioner.")
+    cloudinitd = get_cloudinit(p, c, m, run_name)
 
-    return prov_ids + ids
+    # First, instruct the provisioner to kill all the nodes.
+    try:
+        provisioner = cloudinitd.get_service("provisioner")
+    except IncompatibleEnvironment, e:
+        raise IncompatibleEnvironment("Problem finding the provisioner node in cloudinit.d, "
+                                      "cannot terminate workers without it: %s" % str(e))
+
+    cmd = provisioner.get_ssh_command()
+
+    # TODO: generalize
+    cmd += " 'cd /home/cc/ioncore-python && sudo ./start-killer.sh'"
+
+    if not _run_one_cmd(c, cmd):
+        raise UnexpectedError("Problem triggering worker termination via the provisioner node, "
+                                      "you need to make sure these are terminated manually!")
+
+    # TODO: here, we need to make sure the provisioner is done killing things with some mechanism.
+    #       This will require some thought and design.  For now, this happens fairly instantly if
+    #       the IaaS service is available, etc.  But we should know for sure before proceeding.
+
+    c.log.info("Sent signal to the provisioner, waiting for it to terminate all workers in run '%s'" % run_name)
+    time.sleep(5)
+
+    services = cloudinitd.get_all_services()
+    for service in services:
+        c.log.info("Shutting down '%s'" % service.name)
+        service.shutdown()
+    c.log.info("Shutdown all services launched by cloudinit.d for '%s'" % run_name)
+    c.log.info("You may to run this command now to clean house: cloudinitd terminate '%s'" % run_name)
+
+def _run_one_cmd(c, cmd):
+    c.log.debug("command = '%s'" % cmd)
+    timeout = 30.0 # seconds
+    (killed, retcode, stdout, stderr) = child.child(cmd, timeout=timeout)
+
+    if killed:
+        c.log.error("TIMED OUT: '%s'" % cmd)
+        return False
+
+    if retcode == 0:
+        c.log.debug("command succeeded: '%s'" % cmd)
+        return True
+    else:
+        errmsg = "problem running command, "
+        if retcode < 0:
+            errmsg += "killed by signal:"
+        if retcode > 0:
+            errmsg += "exited non-zero:"
+        errmsg += "'%s' ::: return code" % cmd
+        errmsg += ": %d ::: error:\n%s\noutput:\n%s" % (retcode, stdout, stderr)
+        c.log.error(errmsg)
+        return False
