7e2502fc9ba4d4835137e43805cc844ca2d63cba
oldpatricka <patricka@uvic.ca>
2011-06-06 13:18:05 -0700

    Fix awkward commit message

--- src/python/epumgmt/main/em_core_status.py
+++ src/python/epumgmt/main/em_core_status.py
@@ -117,7 +117,8 @@ def _update_controller_states(c, m, run_name, controller_map, controller_state_m
     for instanceid in controller_map.keys():
         vm = _get_vm_with_instanceid(instanceid, allvms)
         if not vm:
-            raise ProgrammingError("instanceid in RunVMs that is not in controller_map?")
+            msg = "instanceid '%s' is in your controller_map, but not your list of VMs?" % instanceid
+            raise ProgrammingError(msg)
 
         any_newevent = False
         controllers = controller_map[instanceid]
