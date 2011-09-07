cb6ead65bbdf20b36ba643b7ddea3f0f3d790014
Patrick Armstrong <patricka@uvic.ca>
2011-09-07 10:00:04 -0700

    Bump required cloudinit.d version to 1.0

--- setup.py
+++ setup.py
@@ -68,6 +68,6 @@ setup(name="epumgmt",
               "epumgmt = epumgmt.main.em_cmdline:main"
           ]
       },
-      install_requires = ["cloudinitd ==  1.0rc1", "cloudyvents == 0.1", "cloudminer == 0.2"],
+      install_requires = ["cloudinitd ==  1.0", "cloudyvents == 0.1", "cloudminer == 0.2"],
       data_files = data_files
 )
